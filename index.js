7b81c4db8641cf2f5f304c430a3c2ade784b4d70
ruicirilo4 <100714573+ruicirilo4@users.noreply.github.com>
2023-09-20 18:10:39 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const puppeteer = require("puppeteer");
-const express = require("express");
-const app = express();
-const port = 56189;
+import express from "express";
+import axios from "axios";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-app.use(express.static("public"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Substitua 'YOUR_BROWSERLESS_API_KEY' pela sua chave de API do Browserless
+const browserlessApiKey = '9883aff7-00df-4cef-a4e2-e583303a1975';
+const browserlessEndpoint = `https://your-browserless-url.com?key=${browserlessApiKey}`;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -11,29 +19,32 @@ app.get("/", (req, res) => {
 
 app.get("/now-playing", async (req, res) => {
   try {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-
-    await page.goto(
-      "https://indie88.com/wp-content/themes/indie88/inc/streamon.php"
-    );
-
-    await page.waitForSelector(".cobrp-ticker-info", { timeout: 180000 });
-
-    const grabNowPlayingInfo = await page.evaluate(() => {
-      const nowPlayingElement = document.querySelector(".cobrp-ticker-info");
-      const artistElement = nowPlayingElement.querySelector(".cobrp-ticker-artist");
-      const songElement = nowPlayingElement.querySelector(".cobrp-ticker-songtitle");
-
-      const artistText = artistElement ? artistElement.textContent.trim() : "";
-      const songText = songElement ? songElement.textContent.trim() : "";
-
-      return `${artistText} - ${songText}`;
+    const response = await axios.get(`${browserlessEndpoint}/goto`, {
+      params: {
+        url: "https://indie88.com/wp-content/themes/indie88/inc/streamon.php",
+      },
     });
 
-    await browser.close();
+    if (response.status !== 200) {
+      throw new Error("Failed to load the page");
+    }
 
-    res.send(grabNowPlayingInfo);
+    const nowPlayingInfo = await axios.get(`${browserlessEndpoint}/evaluate`, {
+      params: {
+        expression: `
+          const nowPlayingElement = document.querySelector(".cobrp-ticker-info");
+          const artistElement = nowPlayingElement.querySelector(".cobrp-ticker-artist");
+          const songElement = nowPlayingElement.querySelector(".cobrp-ticker-songtitle");
+
+          const artistText = artistElement ? artistElement.textContent.trim() : "";
+          const songText = songElement ? songElement.textContent.trim() : "";
+
+          artistText + " - " + songText;
+        `,
+      },
+    });
+
+    res.send(nowPlayingInfo.data);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Error");
@@ -43,3 +54,4 @@ app.get("/now-playing", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
